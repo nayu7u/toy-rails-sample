29fcf9d9bc6c8a4533b5cf66d39621228c795c11
nayu <19748682+nayu7u@users.noreply.github.com>
2025-03-31 07:50:35 +0000

    feat: implement ghost_form_controller.js

--- app/javascript/controllers/ghost_form_controller.js
+++ app/javascript/controllers/ghost_form_controller.js
@@ -2,6 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="ghost-form"
 export default class extends Controller {
-  connect() {
+  static targets = [ "originalForm", "ghostForm" ]
+
+  submit(event) {
+    const formData = new FormData(this.originalFormTarget)
+    formData.delete("_method")
+    formData.delete("authenticity_token")
+
+    for (const [key, value] of formData.entries()) {
+      const ghost_key = "ghost_" + key
+      const input = this.ghostFormTarget.querySelector(`input[name="${ghost_key}"]`)
+      if (input) {
+        input.value = value;
+      }
+    }
+    this.ghostFormTarget.requestSubmit()
   }
 }
